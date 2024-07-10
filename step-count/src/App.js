49f95ae1b4bf085bed1eb502039496fd58cc8e1f
Fred Coble <fcoble@westga.edu>
2024-07-10 14:27:06 -0400

    Date Counter #76 Completed

--- step-count/src/App.js
+++ step-count/src/App.js
@@ -21,19 +21,25 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
+
+  function handleReset() {
+    setCount(0);
+    setStep(1)
+  }
+
   const date = new Date();
   date.setDate(date.getDate() + count);
 
   return <div>
     <div>
       <div>
-        <button onClick={() => setStep(s=>s-1)}>-</button>
-          <span>Step: {step}</span>
-        <button onClick={() => setStep(s=>s+1)}>+</button>
+        <input type="range" min="0" max="10" value={step} onChange={e=>setStep(Number(e.target.value))}/>
+                  <span>Step: {step}</span>
       </div>
       <div>
+
         <button onClick={() => setCount(c=>c-step)}>-</button>
-          <span>Count: {count}</span>
+        <input type="text" value={count} onChange={e=>setCount(Number(e.target.value))} />
         <button onClick={() => setCount(c=>c+step)}>+</button>
       </div>
       <p>
@@ -44,7 +50,11 @@ function Counter() {
           {date.toDateString()}
         </span>
       </p>
-
+      {(count !== 0 || step !== 1) ?
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div> : null}
+      
     </div>
   </div>
 }
