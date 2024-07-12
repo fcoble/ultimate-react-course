2dbe569b22a07f3a2c1cec3ce1399428ca5a7f32
Fred Coble <fcoble@westga.edu>
2024-07-12 12:50:30 -0400

    #83 Complex Immutable Data Operation

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -3,13 +3,32 @@ import { useState } from "react";
 const initialItems = [
   {id: 1, description: "Passports", quantity: 2,  packed: false},
   {id: 2, description: "Socks", quantity: 12,  packed: true},
-  {id: 2, description: "Charger", quantity: 1,  packed: false},
+  {id: 3, description: "Charger", quantity: 1,  packed: false},
 ]
-export default function app() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(((items) => [...items, item]));
+  }
+
+  function handleDeleteItem(id) {
+    setItems(items => items.filter(item=>item.id !== id))
+  }
+  
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) => 
+        item.id === id ? { ...item, packed: !item.packed }
+    : item)
+  );
+  }
+
   return <div className="app">
     <Logo />
-    <Form />
-    <PackingList />
+    <Form onAddItems={handleAddItems}/>
+    <PackingList items={items} onDeleteItem=
+    {handleDeleteItem} onToggleItem={handleToggleItem}/>
     <Stats />
   </div>
 }
@@ -18,7 +37,7 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>
 }
 
-function Form() {
+function Form({ onAddItems }) {
   
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(5);
@@ -30,6 +49,8 @@ function Form() {
 
     const newItem = {description, quantity, packed: false, id: Date.now()};
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -54,21 +75,22 @@ function Form() {
   </form>;
 }
 
-function PackingList() {
+function PackingList({items, onDeleteItem, onToggleItem}) {
   return (<div className="list">
   <ul >
-    {initialItems.map((item) => (<Item item={item} key={item.id}/>))}
+    {items.map((item) => (<Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>))}
   </ul>
   </div>);
 }
 
 // function Item(props) { 
-function Item({item}) { //object destructuring
+function Item({item, onDeleteItem, onToggleItem}) { //object destructuring
   return <li>
+    <input type="checkbox" value={item.checked} onChange={() => {onToggleItem(item.id)}}/>
     <span   style={item.packed ? {textDecoration: 'line-through'} : {}}>  
     {item.quantity} {item.description}
     </span>
-    <button>❌</button>
+    <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>;
 }
 
